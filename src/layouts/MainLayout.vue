0324d3719aef79909a5b077625ce198f3133eeda
Muhammad Rivandra <rivandra0@gmail.com>
2023-03-24 18:48:42 +0700

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container class="col-6">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  setup () {
+    return {
+    }
+  }
+})
+</script>
